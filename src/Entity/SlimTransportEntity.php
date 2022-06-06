7cfd2ab2e127371b0d9c63211f5d2b914bbc5093
Ben Pitman <pitman.ben@gmail.com>
2022-06-06 03:49:42 +0000

    Updated to kentron3 and php8.0

--- src/Entity/SlimTransportEntity.php
+++ src/Entity/SlimTransportEntity.php
@@ -2,43 +2,57 @@
 
 namespace Kentron\Entity;
 
-use Nyholm\Psr7\ServerRequest;
+use Psr\Http\Message\ServerRequestInterface;
+
+use Slim\Interfaces\RouteInterface;
 use Slim\Routing\RouteContext;
 
 final class SlimTransportEntity extends TransportEntity
 {
-    /**
-     * Slim route context
-     *
-     * @var RouteContext
-     */
-    protected $route;
+    /** The slim route context */
+    protected RouteInterface $route;
 
-    /**
-     * The name of the route
-     *
-     * @var string|null
-     */
-    protected $routeName;
+    /** The name of the route */
+    protected string|null $routeName = null;
 
     /**
      * Getters
      */
+
+    public function getRoute(): RouteInterface
+    {
+        return $this->route;
+    }
 
     public function getRouteName (): ?string
     {
         return $this->routeName;
     }
 
+    public function getRouteArgument(string $key): ?string
+    {
+        return $this->route->getArgument($key);
+    }
+
+    public function getRouteArguments(): array
+    {
+        return $this->route->getArguments();
+    }
+
     /**
      * Setters
      */
 
-    public function setRequest (ServerRequest &$request): void
+    public function setRequest (ServerRequestInterface &$request): void
     {
-        parent::{__FUNCTION__}($request);
+        parent::setRequest($request);
 
         $this->route = RouteContext::fromRequest($this->request)->getRoute();
         $this->routeName = $this->route->getName() ?? null;
+    }
+
+    public function setRouteName(?string $name = null): void
+    {
+        $this->routeName = $name;
     }
 }
